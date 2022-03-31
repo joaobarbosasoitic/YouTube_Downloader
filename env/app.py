aaf9084b6b408add4ed6c67f3eafafe8c2be491b
joaobarbosasoitic <joao.barbosa@soitic.com>
2022-03-31 14:01:10 -0300

    Primeira Alteração

--- env/app.py
+++ env/app.py
@@ -1,6 +1,6 @@
 #-*- coding: utf-8 -*-
 
-
+from time import sleep
 from pytube import YouTube
 from pytube import Playlist
 from pytube import Channel
@@ -12,6 +12,21 @@ def SingleVideoDownload(link):
         print('Baixando Video...')
         yt.streams.first().download('Download_video/')
         print('------ Download Concluido ------')
+        sleep(1000)
+    except Exception as e:
+        print('Erro ao efetuar o download\n', e)
+
+
+def PlaylistDownload(link):
+    try:
+        p = Playlist(link)
+        print(f'baixando playlist {p.title}')
+        for video in p.videos:
+            print(f"Baixando video: {video.title}")
+            video.streams.first().download(f'{p.title}/') 
+            print('------ Download Concluido ------')
+        print('Download efetuado com sucesso!')
+        sleep(1000)
     except Exception as e:
         print('Erro ao efetuar o download\n', e)
 
@@ -21,41 +36,37 @@ def ChannelDownload(link):
         print(f'Baixando Canal: {c.channel_name}')
         for video in c.videos:
             print(f"Baixando video: {video.title}")
-            video.streams.first().download('Download_Lista/')
+            video.streams.first().download(f'{c.channel_name}/')
             print('------ Download Concluido ------')
         print('Download efetuado com sucesso!')
-    except Exception as e:
-        print('Erro ao efetuar o download\n', e)
-
-def PlaylistDownload(link):
-    try:
-        p = Playlist(link)
-        print(f'baixando playlist {p.title}')
-        for video in p.videos:
-            print(f"Baixando video: {video.title}")
-            video.streams.first().download('Download_Canal/') 
-            print('------ Download Concluido ------')
-        print('Download efetuado com sucesso!')
+        sleep(1000)
+        
     except Exception as e:
         print('Erro ao efetuar o download\n', e)
 
 
-print('[1] para baixar um único video')
-print('[2] para baixar uma Lista de Reprodução')
-print('[3] para baixar o canal inteiro')
+print('[1] para baixar um único video\n[2] para baixar uma Lista de Reprodução\n[3] para baixar o canal inteiro')
 print()
 
 op = input("Digite a opção: ")
 
-try:
-    if int(op) == 1:
+if int(op) == 1:
+    try:
         link = input("Digite o link do video: ")
         SingleVideoDownload(link)
-    elif int(op) == 2:
+    except Exception as e:
+        print('Comando não reconhecido\n', e)
+
+elif int(op) == 2:
+    try:
         link = input("Digite o link da Lista de Reprodução: ")
         PlaylistDownload(link)
-    elif int(op) == 3:
+    except Exception as e:
+        print('Comando não reconhecido\n', e)
+
+elif int(op) == 3:
+    try:
         link = input("Digite o link do canal: ")
         ChannelDownload(link)
-except Exception as e:
-    print('Comando não reconhecido\n', e)
+    except Exception as e:
+     print('Comando não reconhecido\n', e)
