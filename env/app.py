e9734252b75d1a59961021f9300dbb933f250eec
joaobarbosasoitic <joao.barbosa@soitic.com>
2022-04-07 12:14:01 -0300

    add classe Youtube_Download

--- env/app.py
+++ env/app.py
@@ -1,72 +1,91 @@
 #-*- coding: utf-8 -*-
 
+
 from time import sleep
 from pytube import YouTube
 from pytube import Playlist
 from pytube import Channel
 
-
-def SingleVideoDownload(link):
-    try:
-        yt = YouTube(link)
-        print('Baixando Video...')
-        yt.streams.first().download('Download_video/')
-        print('------ Download Concluido ------')
-        sleep(1000)
-    except Exception as e:
-        print('Erro ao efetuar o download\n', e)
-
-
-def PlaylistDownload(link):
-    try:
-        p = Playlist(link)
-        print(f'baixando playlist {p.title}')
-        for video in p.videos:
-            print(f"Baixando video: {video.title}")
-            video.streams.first().download(f'{p.title}/') 
+class Youtube_Download:
+    def SingleVideoDownload(link):
+        try:
+            yt = YouTube(link)
+            stream = yt.streams.get_highest_resolution()
+            print('Baixando Video...')
+            stream.download()
             print('------ Download Concluido ------')
-        print('Download efetuado com sucesso!')
-        sleep(1000)
-    except Exception as e:
-        print('Erro ao efetuar o download\n', e)
+            Youtube_Download.opcoes()
+        except KeyboardInterrupt:
+            print('Download cancelado pelo usuario!')
+            print('\n\n')
+            Youtube_Download.opcoes()
+        finally:
+            print('Erro ao efetuar o download\n')
+            print('\n\n')
+            Youtube_Download.opcoes()
 
-def ChannelDownload(link):
-    try:
-        c = Channel(link)
-        print(f'Baixando Canal: {c.channel_name}')
-        for video in c.videos:
-            print(f"Baixando video: {video.title}")
-            video.streams.first().download(f'{c.channel_name}/')
-            print('------ Download Concluido ------')
-        print('Download efetuado com sucesso!')
-        sleep(1000)
-        
-    except Exception as e:
-        print('Erro ao efetuar o download\n', e)
+    def PlaylistDownload(link):
+        try:
+            p = Playlist(link)
+            print(f'baixando playlist {p.title}')
+            for video in p.videos:
+                stream = video.streams.get_highest_resolution()
+                print(f"video: {video.title}")
+                print('Download em andamento...')
+                stream.download(f'{p.title}/') 
+                print('------ Download Concluido ------')
+            print('Download efetuado com sucesso!')
+            print('\n\n')
+            Youtube_Download.opcoes()
 
+        except KeyboardInterrupt:
+            print('Download cancelado pelo usuario!')
+            print('\n\n')
+            Youtube_Download.opcoes()
+        finally:
+            print("Erro ao efetuar download!\n")
 
-print('[1] para baixar um único video\n[2] para baixar uma Lista de Reprodução\n[3] para baixar o canal inteiro')
-print()
+    def ChannelDownload(link):
+        try:
+            c = Channel(link)
+            print(f'Baixando Canal: {c.channel_name}')
+            for video in c.videos:
+                stream = video.streams.get_highest_resolution()
+                print(f"Baixando video: {video.title}")
+                print('Download em andamento...')
+                stream.download(f'{c.channel_name}/')
+                print('------ Download Concluido ------')
+            print('Download efetuado com sucesso!')
+            print('\n\n')
+            Youtube_Download.opcoes()
+            
+        except KeyboardInterrupt:
+            print('Download cancelado pelo usuario!')
+            print('\n\n')
+            Youtube_Download.opcoes()
+        finally:
+            print("Erro ao efetuar download!\n")
 
-op = input("Digite a opção: ")
+    def opcoes():
 
-if int(op) == 1:
-    try:
-        link = input("Digite o link do video: ")
-        SingleVideoDownload(link)
-    except Exception as e:
-        print('Comando não reconhecido\n', e)
+        print('[1] para baixar um único video\n[2] para baixar uma Lista de Reprodução\n[3] para baixar o canal inteiro')
+        print()
+        print('*** Pressione Ctrl - C para cancelar o download ***')
+        print()
 
-elif int(op) == 2:
-    try:
-        link = input("Digite o link da Lista de Reprodução: ")
-        PlaylistDownload(link)
-    except Exception as e:
-        print('Comando não reconhecido\n', e)
+        op = input("Digite a opção: ")
 
-elif int(op) == 3:
-    try:
-        link = input("Digite o link do canal: ")
-        ChannelDownload(link)
-    except Exception as e:
-     print('Comando não reconhecido\n', e)
+        if int(op) == 1:
+            link = input("Digite o link do video: ")
+            Youtube_Download.SingleVideoDownload(link)
+
+        elif int(op) == 2:
+            link = input('Digite o link da lista de reprodução: ')
+            Youtube_Download.PlaylistDownload(link)
+
+        elif int(op) == 3:
+            link = input("Digite o link do canal: ")
+            Youtube_Download.ChannelDownload(link)
+
+if __name__ == "__main__":
+    Youtube_Download.opcoes()
